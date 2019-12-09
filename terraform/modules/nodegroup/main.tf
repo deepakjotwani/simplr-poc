d0f153bf04c3c868a4b78fdf7ba374ca73b46c93
Deepak jotwani <44091069+deepakjotwani@users.noreply.github.com>
2019-12-09 14:53:47 +0530

    Update main.tf

--- terraform/modules/nodegroup/main.tf
+++ terraform/modules/nodegroup/main.tf
@@ -1,5 +1,6 @@
 resource "aws_eks_node_group" "eks_node_group" {
   cluster_name    = var.cluster_name
+  node_role_arn   = aws_iam_role.eks-node-group-iamrole.arn
   node_group_name = var.nodegroup_name
   subnet_ids      = var.subnets
 
@@ -12,9 +13,9 @@ resource "aws_eks_node_group" "eks_node_group" {
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
   depends_on = [
-    aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.example-AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.simplr-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.simplr-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.simplr-AmazonEC2ContainerRegistryReadOnly,
   ]
 }
 
