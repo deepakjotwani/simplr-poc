76a720ad1601ddb09910587e28e77c4a118a4f0b
Deepak jotwani <44091069+deepakjotwani@users.noreply.github.com>
2019-12-09 15:20:36 +0530

    Update main.tf

--- terraform/modules/nodegroup/main.tf
+++ terraform/modules/nodegroup/main.tf
@@ -3,6 +3,7 @@ resource "aws_eks_node_group" "eks_node_group" {
   node_role_arn   = aws_iam_role.eks-node-group-iamrole.arn
   node_group_name = var.nodegroup_name
   subnet_ids      = var.subnets
+  instance_types  = "t2.micro"
 
   scaling_config {
     desired_size = 1
