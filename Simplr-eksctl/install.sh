faeaaca813a08f48fa5d3929fb22bcaa7e09aeb6
Deepak jotwani <44091069+deepakjotwani@users.noreply.github.com>
2019-12-11 09:49:11 +0530

    Create install.sh

---
+++ Simplr-eksctl/install.sh
@@ -0,0 +1,44 @@
+echo "installing aws-iam-authenticator"
+
+curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/linux/amd64/aws-iam-authenticator
+
+chmod +x ./aws-iam-authenticator
+
+mkdir -p $HOME/bin && cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && export PATH=$HOME/bin:$PATH
+
+echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
+
+echo "aws- iam-authenticator succesfully installed"
+
+echo "installing kubectl"
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+
+chmod +x ./kubectl
+
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+kubectl version
+
+echo " kubectl succesfully installed"
+
+echo "installing eksctl"
+
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/download/latest_release/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+
+sudo mv /tmp/eksctl /usr/local/bin
+
+eksctl version
+
+echo " eksctl succesfully installed"
+
+ls
+
+eksctl create cluster -f cluster.yaml
+
+kubectl apply -f deployment.yaml
+
+kubectl apply -f service.yaml
+
+
+
