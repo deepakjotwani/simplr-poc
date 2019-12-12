6e28d166d13bfa50425bcd56d1105582f5a0a520
Deepak jotwani <44091069+deepakjotwani@users.noreply.github.com>
2019-12-12 10:40:35 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
-sudo yum update -y
-
 echo "installing aws-iam-authenticator"
 
 curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/linux/amd64/aws-iam-authenticator
