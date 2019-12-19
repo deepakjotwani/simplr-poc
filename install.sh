637260741aef5286e7ea2a32742f558c52652caf
Deepak jotwani <44091069+deepakjotwani@users.noreply.github.com>
2019-12-19 10:48:09 +0530

    Updated

--- install.sh
+++ install.sh
@@ -34,20 +34,20 @@ echo " eksctl succesfully installed"
 
 
 
-aws cloudformation create-stack --stack-name roles-infra --region us-east-2 --template-body file://infra/roles.yaml --capabilities  CAPABILITY_IAM  CAPABILITY_NAMED_IAM  CAPABILITY_AUTO_EXPAND
+aws cloudformation create-stack --stack-name rolesinfra --region us-east-2 --template-body file://infra/roles.yaml --capabilities  CAPABILITY_IAM  CAPABILITY_NAMED_IAM  CAPABILITY_AUTO_EXPAND
 
-aws cloudformation create-stack --stack-name simplr-nlb-infra --region us-east-2 --template-body file://infra/nlb-infra.yaml --parameters file://infra/parameters.json --capabilities  CAPABILITY_IAM  CAPABILITY_NAMED_IAM  CAPABILITY_AUTO_EXPAND
+aws cloudformation create-stack --stack-name nlbinfra --region us-east-2 --template-body file://infra/nlb-infra.yaml --capabilities  CAPABILITY_IAM  CAPABILITY_NAMED_IAM  CAPABILITY_AUTO_EXPAND
 
 eksctl create cluster -f cluster.yaml
 
-aws cloudformation create-stack --stack-name simplr-nlb-alb --region us-east-2 --template-body file://infra/nlb-alb.json --parameters file://infra/parameters.json --capabilities  CAPABILITY_IAM  CAPABILITY_NAMED_IAM  CAPABILITY_AUTO_EXPAND
+# aws cloudformation create-stack --stack-name simplr-nlb-alb --region us-east-2 --template-body file://infra/nlb-alb.json --capabilities  CAPABILITY_IAM  CAPABILITY_NAMED_IAM  CAPABILITY_AUTO_EXPAND
 
 
 
- kubectl apply -f ./accesscontrol
+#  kubectl apply -f ./accesscontrol
 
- kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.4/docs/examples/rbac-role.yaml
- kubectl apply -f ./ingress-controller/alb-ingress-controller.yaml
+#  kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.4/docs/examples/rbac-role.yaml
+#  kubectl apply -f ./ingress-controller/alb-ingress-controller.yaml
 
 
 # kubectl apply -f./Frontend
