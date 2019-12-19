b935b9e69a0301793c7ea084658198013f336eae
Deepak jotwani <44091069+deepakjotwani@users.noreply.github.com>
2019-12-19 17:22:03 +0530

    Update variables.sh

--- variables.sh
+++ variables.sh
@@ -1,5 +1,5 @@
 #!/bin/sh 
-export CLUSTER_ROLE_ARN="aws cloudformation --region us-east-2 list-exports  | jq '.Exports[] | select(.Name == "rolesinfra-ClusterRoleARN") | .Value' | tr -d \'\"" | sed -i "s|CLUSTER_ROLE_ARN|${CLUSTER_ROLE_AR}|" cluster.yaml
+export CLUSTER_ROLE_ARN="aws cloudformation --region us-east-2 list-exports  | jq '.Exports[] | select(.Name == "rolesinfra-ClusterRoleARN") | .Value' | tr -d \'\"" | sed -i "s|CLUSTER_ROLE_ARN|${CLUSTER_ROLE_ARN}|" cluster.yaml
 export NODEGROUP_ROLE_ARN="aws cloudformation --region us-east-2 list-exports  | jq '.Exports[] | select(.Name == "rolesinfra-NodeGroupRoleARN") | .Value' | tr -d \'\"" | sed -i "s|NODEGROUP_ROLE_ARN|${NODEGROUP_ROLE_ARN}|" cluster.yaml
 export INSTANCE_PROFILE_ARN="aws cloudformation --region us-east-2 list-exports  | jq '.Exports[] | select(.Name == "rolesinfra-NodeGroupInstanceProfileARN") | .Value' | tr -d \'\"" | sed -i "s|INSTANCE_PROFILE_ARN|${INSTANCE_PROFILE_ARN}|" cluster.yaml
 
