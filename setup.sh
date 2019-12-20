02107a20188e53041b43445cd166167e83c96632
Deepak jotwani <44091069+deepakjotwani@users.noreply.github.com>
2019-12-20 10:47:27 +0530

    updates variables

--- setup.sh
+++ setup.sh
@@ -1,31 +1,31 @@
 
 aws cloudformation create-stack --stack-name rolesinfra --region us-east-2 --template-body file://infra/roles.yaml --capabilities  CAPABILITY_IAM  CAPABILITY_NAMED_IAM  CAPABILITY_AUTO_EXPAND
 
+ aws cloudformation wait stack-create-complete --region us-east-2 --stack-name rolesinfra
+
 aws cloudformation create-stack --stack-name nlbinfra --region us-east-2 --template-body file://infra/nlb-infra.yaml --capabilities  CAPABILITY_IAM  CAPABILITY_NAMED_IAM  CAPABILITY_AUTO_EXPAND
 
-until aws cloudformation --region us-east-2 list-exports | grep -q ""rolesinfra"";
-do 
-  sleep 1; 
-done
+ aws cloudformation wait stack-create-complete --region us-east-2 --stack-name nlbinfra 
+
+aws cloudformation --region us-east-2 list-exports
+
+./varibles.sh
+
+cat cluster.yaml
 
 
-./variables.sh
+# eksctl create cluster -f cluster.yaml
 
-eksctl create cluster -f cluster.yaml
-
-# aws cloudformation create-stack --stack-name simplr-nlb-alb --region us-east-2 --template-body file://infra/nlb-alb.json --capabilities  CAPABILITY_IAM  CAPABILITY_NAMED_IAM  CAPABILITY_AUTO_EXPAND
-
-
-
-
-
-kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.4/docs/examples/rbac-role.yaml
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.4/docs/examples/rbac-role.yaml
   
-kubectl apply -f ./ingress-controller/alb-ingress-controller.yaml
+# kubectl apply -f ./ingress-controller/alb-ingress-controller.yaml
 
 #  kubectl apply -f ./accesscontrol
- kubectl apply -f./Frontend
- kubectl apply -f./Backend
- kubectl apply -f ./ingress-controller/ingress.yaml
+#  kubectl apply -f./Frontend
+#  kubectl apply -f./Backend
+#  kubectl apply -f ./ingress-controller/ingress.yaml
+
+
+# aws cloudformation create-stack --stack-name simplr-nlb-alb --region us-east-2 --template-body file://infra/nlb-alb.json --capabilities  CAPABILITY_IAM  CAPABILITY_NAMED_IAM  CAPABILITY_AUTO_EXPAND
 
 
--- variables.sh
+++ variables.sh
@@ -1,7 +1,4 @@
 #!/bin/sh 
-export CLUSTER_ROLE_ARN="aws cloudformation --region us-east-2 list-exports  | jq '.Exports[] | select(.Name == "rolesinfra-ClusterRoleARN") | .Value' | tr -d \'\"" | sed -i "s|CLUSTER_ROLE_ARN|${CLUSTER_ROLE_ARN}|" cluster.yaml
-export NODEGROUP_ROLE_ARN="aws cloudformation --region us-east-2 list-exports  | jq '.Exports[] | select(.Name == "rolesinfra-NodeGroupRoleARN") | .Value' | tr -d \'\"" | sed -i "s|NODEGROUP_ROLE_ARN|${NODEGROUP_ROLE_ARN}|" cluster.yaml
-export INSTANCE_PROFILE_ARN="aws cloudformation --region us-east-2 list-exports  | jq '.Exports[] | select(.Name == "rolesinfra-NodeGroupInstanceProfileARN") | .Value' | tr -d \'\"" | sed -i "s|INSTANCE_PROFILE_ARN|${INSTANCE_PROFILE_ARN}|" cluster.yaml
-
-
-
+export CLUSTER_ROLE_ARN="$(aws cloudformation --region us-east-2 list-exports  | jq '.Exports[] | select(.Name == "rolesinfra-ClusterRoleARN") | .Value' | tr -d \'\")" | sed -i "s|CLUSTER_ROLE_ARN|${CLUSTER_ROLE_ARN}|" cluster.yaml
+export NODEGROUP_ROLE_ARN="$(aws cloudformation --region us-east-2 list-exports  | jq '.Exports[] | select(.Name == "rolesinfra-NodeGroupRoleARN") | .Value' | tr -d \'\")" | sed -i "s|NODEGROUP_ROLE_ARN|${NODEGROUP_ROLE_ARN}|" cluster.yaml
+export INSTANCE_PROFILE_ARN="$(aws cloudformation --region us-east-2 list-exports  | jq '.Exports[] | select(.Name == "rolesinfra-NodeGroupInstanceProfileARN") | .Value' | tr -d \'\")" | sed -i "s|INSTANCE_PROFILE_ARN|${INSTANCE_PROFILE_ARN}|" cluster.yaml
