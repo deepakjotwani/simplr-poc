46655dd8ed94c6c83cd133acf91b30207b37c58c
Deepak jotwani <44091069+deepakjotwani@users.noreply.github.com>
2019-12-25 20:09:09 +0530

    Update variables.sh

--- variables.sh
+++ variables.sh
@@ -1,13 +1,13 @@
 #!/bin/sh 
 export CLUSTER_ROLE_ARN="$(aws cloudformation --region REGION1 list-exports  | jq '.Exports[] | select(.Name == "ROLE_STACK-ClusterRoleARN") | .Value' | tr -d \'\")" 
 sed -i "s|CLUSTER_ROLE_ARN|${CLUSTER_ROLE_ARN}|" cluster.yaml
-export NODEGROUP_ROLE_ARN="$(aws cloudformation --region REGION1 list-exports  | jq '.Exports[] | select(.Name == "'ROLE_STACK'-NodeGroupRoleARN") | .Value' | tr -d \'\")" 
+export NODEGROUP_ROLE_ARN="$(aws cloudformation --region REGION1 list-exports  | jq '.Exports[] | select(.Name == "ROLE_STACK-NodeGroupRoleARN") | .Value' | tr -d \'\")" 
 sed -i "s|NODEGROUP_ROLE_ARN|${NODEGROUP_ROLE_ARN}|" cluster.yaml
-export INSTANCE_PROFILE_ARN="$(aws cloudformation --region REGION1 list-exports  | jq '.Exports[] | select(.Name == "'ROLE_STACK'-NodeGroupInstanceProfileARN") | .Value' | tr -d \'\")" 
+export INSTANCE_PROFILE_ARN="$(aws cloudformation --region REGION1 list-exports  | jq '.Exports[] | select(.Name == "ROLE_STACK-NodeGroupInstanceProfileARN") | .Value' | tr -d \'\")" 
 sed -i "s|INSTANCE_PROFILE_ARN|${INSTANCE_PROFILE_ARN}|" cluster.yaml
 
 
-export VPC_ID="$(aws cloudformation --region REGION1 list-exports  | jq '.Exports[] | select(.Name == "'NETWORK_STACK':vpc:id") | .Value'  | tr -d \'\")"
+export VPC_ID="$(aws cloudformation --region REGION1 list-exports  | jq '.Exports[] | select(.Name == "NETWORK_STACK:vpc:id") | .Value'  | tr -d \'\")"
 sed -i "s|VPC_ID|${VPC_ID}|" cluster.yaml
 
 export AZ1="$(aws cloudformation --region REGION1 list-exports  | jq '.Exports[] | select(.Name == "'$VPC_ID':availability-zone-1:id") | .Value'  | tr -d \'\")"
