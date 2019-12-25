b7009be4f8d8272cf1fb29326d2932412b759987
Deepak jotwani <44091069+deepakjotwani@users.noreply.github.com>
2019-12-25 19:53:48 +0530

    Update variables.sh

--- variables.sh
+++ variables.sh
@@ -1,24 +1,24 @@
 #!/bin/sh 
-export CLUSTER_ROLE_ARN="$(aws cloudformation --region REGION list-exports  | jq '.Exports[] | select(.Name == "ROLE_STACK-ClusterRoleARN") | .Value' | tr -d \'\")" 
+export CLUSTER_ROLE_ARN="$(aws cloudformation --region REGION1 list-exports  | jq '.Exports[] | select(.Name == "ROLE_STACK-ClusterRoleARN") | .Value' | tr -d \'\")" 
 sed -i "s|CLUSTER_ROLE_ARN|${CLUSTER_ROLE_ARN}|" cluster.yaml
-export NODEGROUP_ROLE_ARN="$(aws cloudformation --region REGION list-exports  | jq '.Exports[] | select(.Name == "'ROLE_STACK'-NodeGroupRoleARN") | .Value' | tr -d \'\")" 
+export NODEGROUP_ROLE_ARN="$(aws cloudformation --region REGION1 list-exports  | jq '.Exports[] | select(.Name == "'ROLE_STACK'-NodeGroupRoleARN") | .Value' | tr -d \'\")" 
 sed -i "s|NODEGROUP_ROLE_ARN|${NODEGROUP_ROLE_ARN}|" cluster.yaml
-export INSTANCE_PROFILE_ARN="$(aws cloudformation --region REGION list-exports  | jq '.Exports[] | select(.Name == "'ROLE_STACK'-NodeGroupInstanceProfileARN") | .Value' | tr -d \'\")" 
+export INSTANCE_PROFILE_ARN="$(aws cloudformation --region REGION1 list-exports  | jq '.Exports[] | select(.Name == "'ROLE_STACK'-NodeGroupInstanceProfileARN") | .Value' | tr -d \'\")" 
 sed -i "s|INSTANCE_PROFILE_ARN|${INSTANCE_PROFILE_ARN}|" cluster.yaml
 
 
-export VPC_ID="$(aws cloudformation --region REGION list-exports  | jq '.Exports[] | select(.Name == "'NETWORK_STACK':vpc:id") | .Value'  | tr -d \'\")"
+export VPC_ID="$(aws cloudformation --region REGION1 list-exports  | jq '.Exports[] | select(.Name == "'NETWORK_STACK':vpc:id") | .Value'  | tr -d \'\")"
 sed -i "s|VPC_ID|${VPC_ID}|" cluster.yaml
 
-export AZ1="$(aws cloudformation --region REGION list-exports  | jq '.Exports[] | select(.Name == "'$VPC_ID':availability-zone-1:id") | .Value'  | tr -d \'\")"
+export AZ1="$(aws cloudformation --region REGION1 list-exports  | jq '.Exports[] | select(.Name == "'$VPC_ID':availability-zone-1:id") | .Value'  | tr -d \'\")"
 sed -i "s|AZ1|${AZ1}|" cluster.yaml
 
-export AZ2="$(aws cloudformation --region REGION list-exports  | jq '.Exports[] | select(.Name == "'$VPC_ID':availability-zone-2:id") | .Value'  | tr -d \'\")"
+export AZ2="$(aws cloudformation --region REGION1 list-exports  | jq '.Exports[] | select(.Name == "'$VPC_ID':availability-zone-2:id") | .Value'  | tr -d \'\")"
 sed -i "s|AZ2|${AZ2}|" cluster.yaml
-export AZ3="$(aws cloudformation --region REGION list-exports  | jq '.Exports[] | select(.Name == "'$VPC_ID':availability-zone-3:id") | .Value'  | tr -d \'\")"
+export AZ3="$(aws cloudformation --region REGION1 list-exports  | jq '.Exports[] | select(.Name == "'$VPC_ID':availability-zone-3:id") | .Value'  | tr -d \'\")"
 sed -i "s|AZ3|${AZ3}|" cluster.yaml
                  
-export PRIVATE_SUBNET_IDS="$(aws cloudformation --region REGION list-exports  | jq '.Exports[] | select(.Name == "'$VPC_ID':private-subnet:ids") | .Value' | tr -d \'\")"
+export PRIVATE_SUBNET_IDS="$(aws cloudformation --region REGION1 list-exports  | jq '.Exports[] | select(.Name == "'$VPC_ID':private-subnet:ids") | .Value' | tr -d \'\")"
 
 export SUBNET1="$(echo $PRIVATE_SUBNET_IDS |  cut -d',' -f1)"
 sed -i "s|SUBNET1|${SUBNET1}|" cluster.yaml
