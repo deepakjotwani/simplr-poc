bae891b80978a77a181930aa18c00d6d62eafb31
Deepak jotwani <44091069+deepakjotwani@users.noreply.github.com>
2019-12-26 13:45:35 +0530

    simpplr->smtip

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-aws s3 cp s3://simplrinfrabucket/eks/cluster.yaml .
-aws s3 cp s3://simplrinfrabucket/vpc/cf_template_master.yaml .
-aws s3 cp s3://simplrinfrabucket/vpc/parameters.json  .
+aws s3 cp s3://smtip-artifact-bucket/eks/cluster.yaml .
+aws s3 cp s3://smtip-artifact-bucket/vpc/cf_template_master.yaml .
+aws s3 cp s3://smtip-artifact-bucket/vpc/parameters.json  .
 
 
 aws cloudformation create-stack --stack-name $2 --region $1  --template-body file://cf_template_master.yaml --parameters file://parameters.json --capabilities  CAPABILITY_IAM  CAPABILITY_NAMED_IAM  CAPABILITY_AUTO_EXPAND
